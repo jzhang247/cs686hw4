4c8893002226280e59f36614146f80792cab633d
jzhang247 <=>
2025-02-05 14:35:52 -0800

    Implemented compose

---
+++ mysql/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    cover VARCHAR(255),
+    price VARCHAR(255)
+);
